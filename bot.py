5bf07fed5c6fb0c49163ee506d5be91764f4bee0
Starenesha <olgastarynenko@gmail.com>
2020-06-23 18:29:08 +0300

    some changes

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import re
 from config import TOKEN
 import time
 
-bot = telebot.AsyncTeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN)
 task = bot.get_me()
 pattern = '((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)'
 
@@ -12,20 +12,17 @@ pattern = '((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)'
 def start_message(message):
     bot.send_message(message.chat.id, "Hi! Give me site address and I will return you a screenshot of the page.")
 
-@bot.message_handler(content_types=['document'])
+@bot.message_handler(regexp='.*?')
 def answer_messages(message):
     url = re.match(pattern, message.text).group(0)
     filename = time.time()
+
     try:
         img = imgkit.from_url(url, f'screens/{filename}.jpg')
-
-        with open(img, 'rb') as file:
+        with open(f'screens/{filename}.jpg', 'rb') as file:
             bot.send_photo(message.chat.id, file, caption='Your screenshot:', reply_to_message_id=message.message_id)
-            task.wait()
-
     except Exception:
         bot.send_message(message.chat.id, "Something goes wrong, please check your site url")
-        task.wait()
 
 if __name__ == '__main__':
-    bot.polling()
+    bot.polling(none_stop=True)
